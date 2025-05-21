6079181f14577fac33331791eab8a35bf7f9fb12
Stephen Schuez <stephens@hey.com>
2025-05-21 20:59:08 +0200

    Add critial line to enable vim rails

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,7 +25,10 @@ return {
           "yaml",
         },
         auto_install = true,
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = true,
+        },
         indent = { enable = true },
       })
     end,
