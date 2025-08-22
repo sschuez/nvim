84cd383d8b1fa6d5c7c6c2814dba98c575c6a8b8
Stephen Schuez <stephens@hey.com>
2025-08-22 13:51:22 +0200

    feat: add herb-lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,6 +2,28 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      setup = {
+        herb_ls = function(_, opts)
+          local lspconfig = require("lspconfig")
+          local configs = require("lspconfig.configs")
+          
+          -- Define herb_ls as a custom server if not already defined
+          if not configs.herb_ls then
+            configs.herb_ls = {
+              default_config = {
+                cmd = { "herb-language-server", "--stdio" },
+                filetypes = { "eruby" },
+                root_dir = lspconfig.util.root_pattern(".git", "Gemfile", "package.json"),
+                single_file_support = true,
+                name = "herb_ls",
+              },
+            }
+          end
+          
+          lspconfig.herb_ls.setup(opts)
+          return true -- Prevent default setup
+        end,
+      },
       servers = {
         ruby_lsp = {
           mason = false, -- Disable Mason for Ruby LSP
@@ -13,6 +35,9 @@ return {
         rubocop = {
           mason = false,
           cmd = { "bundle", "exec", "rubocop", "--lsp" }, -- Use project RuboCop
+        },
+        herb_ls = {
+          mason = false,
         },
       },
     },
